5eb50cd98dee0e8ed4e111d958c5668aa3bd9d88
Benjamin Fleming <assimilatedgiraffe@gmail.com>
2017-10-09 21:30:11 +0100

    TextEditor and NoteList vue files

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -1,26 +1,44 @@
 <template lang="html">
   <div id="main-app">
     <el-row class="header">
-      <el-col :span="24">history/breadcrumbs for subjects; recents</el-col>
+      <el-col :span="24">
+        history/breadcrumbs for subjects; recents
+        <NoteList/>
+      </el-col>
     </el-row>
     <el-row class="main-section">
-      <el-col :span="4">parent</el-col>
-      <el-col :span="16">main editor</el-col>
-      <el-col :span="4">children</el-col>
+      <el-col :span="4">
+        parent
+        <NoteList/>
+      </el-col>
+      <el-col :span="16">
+        main editor
+        <TextEditor/>
+      </el-col>
+      <el-col :span="4">
+        children
+        <NoteList/>
+      </el-col>
     </el-row>
   </div>
 </template>
 
 
 <script>
+import TextEditor from './TextEditor.vue'
+import NoteList from './NoteList.vue'
+
 export default {
   name: 'MainApp',
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    TextEditor,
+    NoteList
   }
-
 }
 </script>
 
---
+++ src/components/NoteList.vue
@@ -0,0 +1,20 @@
+<template lang="html">
+  <div class="note-list">
+    <el-card v-for="note in notes"></el-card>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      notes: [
+        'one', 'two', 'three'
+      ]
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>

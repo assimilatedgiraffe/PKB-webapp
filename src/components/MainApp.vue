01efc87b2b968447c5cb2ad27b01cbe2d4b3cb14
Benjamin Fleming <assimilatedgiraffe@gmail.com>
2017-10-10 21:29:35 +0100

    test data generator

--- src/components/MainApp.vue
+++ src/components/MainApp.vue
@@ -31,27 +31,28 @@ import NoteList from './NoteList.vue'
 
 export default {
   name: 'MainApp',
+
   data () {
     return {
-      notes: {
-        note1: {
-          title: 'Example Title',
-          text: 'some much longer example text for a note to see how scrolling works in the web app fjdklsfjsldkfjldksjf  jslkdfj dsfjkld fjslkdjs;lkfjsl   djklsjdf;lsjflksjfha fhsdjkf ahlkjf hskd  fhjdksla skdfdskjdgfjhdsf ghdj fsdhf sd f df hdskjfsdhkdjfhslkfh s dfhjdkslfksdjf skf ds fhsklf dkjhfskdhf sdfsdhjfklsfksdlfslj df shjkfhs f'
-        },
-        note2: {
-          title: 'Example Title',
-          text: 'some example text for a note'
-        },
-        note3: {
-          title: 'Example Title',
-          text: 'some example text for a note'
-        }
-      }
+      notes: {}
     }
   },
-  components: {
-    TextEditor,
-    NoteList
+
+  components: {TextEditor, NoteList},
+
+  methods: {
+    generateTestData () {
+      let testNotes = {}
+      for (let i = 0; i < 5; i++) {
+        testNotes[i] = {title: 'Text Title', text: 'some example text for a note'}
+      }
+      return testNotes
+    }
+  },
+
+  mounted () {
+    // do something after mounting vue instance
+    this.notes = this.generateTestData()
   }
 }
 </script>

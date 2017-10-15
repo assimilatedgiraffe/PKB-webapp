21fe9901b544a1735c86245dfcb3090fff0710f3
Benjamin Fleming <assimilatedgiraffe@gmail.com>
2017-10-15 22:37:10 +0100

    Basic keyboard control

--- src/components/NoteGrid.vue
+++ src/components/NoteGrid.vue
@@ -1,9 +1,13 @@
 <template lang="html">
-  <div>
-    <el-col :span="colSpan" v-for="col in cols" :key="col.id">
-      <div class="note-list" >
-          <div v-for="note in col" :key="note.id">
-            <TextEditor :note="note"></TextEditor>
+  <div >
+    <el-col  :span="colSpan" v-for="(col, i) in cols" :key="col.id" @keyup="keyboardMap"  >
+      <div class="noteEditor-list" >
+          <div v-for="(noteEditor,j) in col"
+            :key="noteEditor.id">
+            <TextEditor
+              :noteEditor="noteEditor">
+              <!-- @click.native="onNoteClick(i,j)"> -->
+            </TextEditor>
           </div>
       </div>
     </el-col>
@@ -16,30 +20,86 @@ import TextEditor from './TextEditor.vue'
 export default {
   data: -> {
     numberOfCols: 3
-    selectedIndex: [0,0,0]
+    selectedNote: 0
     selectedCol: 0
+    # selectedNote: {}
   }
   props: ['notes']
   components: {TextEditor}
   computed: {
     colSpan: -> 24 / this.numberOfCols
     cols: ->
-      loading = [[{text:""}],[],[]]
       root = this.notes[0]
-      return loading if not root
-      # console.log root.children
-      zero = root.children
+      if not root
+        #Still loading notes from firebase
+        # this.cols[0][0].isSelected = true
+        return [[],[],[]]
+      #  wrap note(data model) in noteEditor(view model)
+      wrap = (note) -> {
+        note
+        isSelected: false
+      }
+      zero = root.children.map wrap
       # one = zero[this.selectedIndex[0]].children?
       # two = one[this.selectedIndex[1]].children?
       # return [[{text:""}], [{text:""}], [{text:""}]]
       return [zero,zero,zero]
   }
-}
+  methods: {
+    # onNoteClick: (i,j) ->
+    #   this.selectedCol = i
+    #   this.selectedNote = j
 
+    keyboardMap: (e) ->
+      console.log e.key
+      col = this.cols[this.selectedCol]
+      console.log this.selectedNote
+      switch e.key
+        when "j"
+          this.cols[this.selectedCol][this.selectedNote].isSelected = false
+          this.selectedNote += 1
+          this.cols[this.selectedCol][this.selectedNote].isSelected = true
+        when "k"
+          col[this.selectedNote].isSelected = false
+          this.selectedNote -= 1
+          col[this.selectedNote].isSelected = true
+        when "h"
+          this.selectedCol -= 1
+        when "l"
+          this.selectedCol += 1
+      # col = this.cols[this.selectedCol]
+      # i = this.selectedNote
+      # if e.key == "j"
+      #   # this.cols[selectedCol].length
+      #   console.log this
+      #   # col[i].isSelected = false
+      #   # i += 1
+      #   this.selectedNote += 1
+      #   this.cols[this.selectedCol][this.selectedNote].isSelected = true
+  }
+  mounted: ->
+    # keyboardMap: (e) ->
+    #   console.log e.key
+    #   # col = this.cols[this.selectedCol]
+    #   # i = this.selectedNote
+    #   if e.key == "j"
+    #     # this.cols[selectedCol].length
+    #     console.log this
+    #     # col[i].isSelected = false
+    #     # i += 1
+    #     this.cols[this.selectedCol][this.selectedNote].isSelected = false
+    #     this.selectedNote += 1
+    #     this.cols[this.selectedCol][this.selectedNote].isSelected = true
+    # console.log keyboardMap()
+    # that = this
+    console.log this.keyboardMap
+    that = this
+    window.addEventListener("keyup", that.keyboardMap)
+}
 </script>
 
 <style lang="css">
-  .note-list {
+  .noteEditor-list {
     overflow-y: scroll;
     height: 100%;
   }
